96b55bc84080dddb31317d2525fd629a7ea2f877
AlexSchunert <a.schunert@gmail.com>
2023-10-08 16:07:24 +0200

    Implemented button to select file and function to read text file

--- app.js
+++ app.js
@@ -1,3 +1,42 @@
+//document.body.onload = addElement;
+
+function import_graphml_file() {
+    let input = document.createElement('input');
+    const new_div = document.createElement("div");
+    
+
+    input.type = 'file';
+    input.onchange = _ => {
+      // you can use this method to get file and perform respective operations
+              let files =   Array.from(input.files);
+              console.log(files);
+              console.log(files[0].type);
+              const load_confirmation = document.createTextNode("Load File: " + files[0].name);
+              new_div.appendChild(load_confirmation);
+              //const current_div = document.getElementById("div1");
+              document.body.appendChild(new_div)
+              load_txt_file(files[0])
+          };
+    input.click();
+
+    
+}
+
+
+function load_txt_file(file) {
+
+    let reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = function() {
+        console.log(reader.result);
+    };
+    
+    reader.onerror = function() {
+        console.log(reader.error);
+    };       
+}
+/*
 function load_button(){
     alert("This is supposed to load a file")
 } 
+*/
