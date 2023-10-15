28fcfa6f8c198ad034eb884b1dbc4a1fde1bf6af
AlexSchunert <a.schunert@gmail.com>
2023-10-15 10:53:44 +0200

    Added autoformatting, made reading of file async

--- app.js
+++ app.js
@@ -1,30 +1,65 @@
 //document.body.onload = addElement;
 
-function import_graphml_file() {
+async function import_graphml_file() {
     let input = document.createElement('input');
     const new_div = document.createElement("div");
-    
 
     input.type = 'file';
-    input.onchange = _ => {
-      // you can use this method to get file and perform respective operations
-              let files =   Array.from(input.files);
-              console.log(files);
-              console.log(files[0].type);
-              const load_confirmation = document.createTextNode("Load File: " + files[0].name);
-              new_div.appendChild(load_confirmation);
-              //const current_div = document.getElementById("div1");
-              document.body.appendChild(new_div)
-              load_txt_file(files[0])
-          };
+    input.onchange = _ => handle_file_input(input, new_div);
     input.click();
+}
 
-    
+async function handle_file_input(input, new_div) {
+    // you can use this method to get file and perform respective operations
+    let files = Array.from(input.files);
+    console.log(files);
+    console.log(files[0].type);
+    const load_confirmation = document.createTextNode("Load File: " + files[0].name);
+    new_div.appendChild(load_confirmation);
+    //const current_div = document.getElementById("div1");
+    document.body.appendChild(new_div)
+
+    try {
+        const file_content = await load_txt_file(files[0]);
+        console.log(file_content)
+
+    }
+    catch (error) {
+        console.error(error);
+
+    }
+
+    /*
+    file_content_promise
+        .then(result => {
+            console.log(result);
+        })
+        .catch(error => {
+            console.error(error);
+        })
+        .finally(() => {
+            console.log("File read")
+        })
+    */
 }
 
 
-function load_txt_file(file) {
+async function load_txt_file(file) {
 
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = () => {
+            resolve(reader.result);
+            //console.log(reader.result);
+        }
+        reader.onerror = () => {
+            reject(reader.error);
+
+        };
+        reader.readAsText(file);
+    })
+
+    /*
     let reader = new FileReader();
     reader.readAsText(file);
     reader.onload = function() {
@@ -33,7 +68,10 @@ function load_txt_file(file) {
     
     reader.onerror = function() {
         console.log(reader.error);
-    };       
+    };
+    return reader
+    */
+
 }
 /*
 function load_button(){
