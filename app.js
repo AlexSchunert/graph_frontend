15b4afb0fce8ea167db301956fc0235f4a7bb15c
AlexSchunert <a.schunert@gmail.com>
2023-10-21 19:18:46 +0200

    added d3.js - issue with nodes not properly displayed

--- app.js
+++ app.js
@@ -43,19 +43,6 @@ async function handle_file_input(input, new_div) {
     catch (error) {
         console.error(error);
     }
-
-    /*
-    file_content_promise
-        .then(result => {
-            console.log(result);
-        })
-        .catch(error => {
-            console.error(error);
-        })
-        .finally(() => {
-            console.log("File read")
-        })
-    */
 }
 
 
@@ -73,23 +60,90 @@ async function load_txt_file(file) {
         };
         reader.readAsText(file);
     })
-
-    /*
-    let reader = new FileReader();
-    reader.readAsText(file);
-    reader.onload = function() {
-        console.log(reader.result);
-    };
-    
-    reader.onerror = function() {
-        console.log(reader.error);
-    };
-    return reader
-    */
-
 }
-/*
-function load_button(){
-    alert("This is supposed to load a file")
-} 
-*/
+
+// Create an empty graph
+var nodes = [];
+var links = [];
+
+// Initialize the D3.js force simulation
+var width = 600;
+var height = 400;
+
+var svg = d3.select("#graph-container")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+var simulation = d3.forceSimulation(nodes)
+    .force("charge", d3.forceManyBody().strength(-300))
+    .force("link", d3.forceLink(links).distance(100))
+    .force("center", d3.forceCenter(600, 400));
+//.force("center", d3.forceCenter(width / 2, height / 2));
+
+// Create functions to add nodes and edges
+d3.select("#add-node").on("click", addNode);
+d3.select("#add-edge").on("click", addEdge);
+
+function addNode() {
+    nodes.push({ id: nodes.length });
+    updateGraph();
+}
+
+function addEdge() {
+    var node1 = parseInt(document.getElementById("node1").value);
+    var node2 = parseInt(document.getElementById("node2").value);
+
+    if (isNaN(node1) || isNaN(node2) || node1 === node2) {
+        alert("Invalid node IDs. Please enter valid IDs.");
+        return;
+    }
+
+    links.push({ source: node1, target: node2 });
+    updateGraph();
+}
+
+function updateGraph() {
+    var link = svg.selectAll(".link")
+        .data(links)
+        .enter()
+        .append("line")
+        .attr("class", "link");
+
+    var node = svg.selectAll(".node")
+        .data(nodes)
+        .enter()
+        .append("circle")
+        .attr("class", "node")
+        .attr("r", 30)
+        .style("fill", "orange")
+        .call(d3.drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended));
+
+    simulation.nodes(nodes);
+    simulation.force("link").links(links);
+    simulation.alpha(1).restart();
+
+    node.exit().remove();
+    link.exit().remove();
+}
+
+function dragstarted(d) {
+    if (!d3.event.active) simulation.alpha(1).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+}
+
+function dragged(d) {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+}
+
+function dragended(d) {
+    if (!d3.event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
+}
+
